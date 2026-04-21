4965b1ca7f806f4c1e8d0c6b942cb3f3a7c916e6
Teppo <teppo.erkkila@gmail.com>
2026-04-21 08:42:34 +0300

    Corrected safety flags for Ioniq 5 HDA1

--- opendbc/car/hyundai/interface.py
+++ opendbc/car/hyundai/interface.py
@@ -77,7 +77,7 @@ class CarInterface(CarInterfaceBase):
         cfgs.insert(0, get_safety_config(structs.CarParams.SafetyModel.noOutput))
       ret.safetyConfigs = cfgs
 
-      if ret.flags & HyundaiFlags.CANFD_LKA_STEER_MSG or ret.carFingerprint == CAR.HYUNDAI_IONIQ_5:
+      if ret.flags & HyundaiFlags.CANFD_LKA_STEER_MSG:
         ret.safetyConfigs[-1].safetyParam |= HyundaiSafetyFlags.CANFD_LKA_STEER_MSG.value
         if ret.flags & HyundaiFlags.CANFD_LKA_STEER_MSG_ALT:
           ret.safetyConfigs[-1].safetyParam |= HyundaiSafetyFlags.CANFD_LKA_STEER_MSG_ALT.value
